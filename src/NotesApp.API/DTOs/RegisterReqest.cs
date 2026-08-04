a56059c43b1ea4fa1c6b40c058fc05a63c93662b
Marokos999 <marokos987@outlook.com>
2026-08-04 22:49:36 +0200

    chore: switch from SQLite to PostgreSQL, remove unused Authorization package

--- src/NotesApp.API/DTOs/RegisterReqest.cs
+++
@@ -1,6 +0,0 @@
-namespace NotesApp.API.DTOs;
-
-public record RegisterReqest(string Email, string Password);
-public  record LoginReqest(string Email, string Password);
-public record RefreshTokenReqest(string RefreshToken);
-public record AuthResponse(string AccessToken, string RefreshToken, DateTime ExpiresAt, string Email);
